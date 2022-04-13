6d9182311d71747e559290c50155a1d53f25425d
Cesar Gutierrez Tineo <a20062161@pucp.edu.pe>
2022-04-12 23:49:39 -0500

    add tf files

---
+++ create-instance.tf
@@ -0,0 +1,56 @@
+# create-instance.tf
+ 
+data "aws_ami" "ubuntu" {
+
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+resource "aws_instance" "instance" {
+  ami                         = data.aws_ami.ubuntu.id
+  availability_zone           = "${var.aws_region}${var.aws_region_az}"
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
+  key_name                    = var.key_pair
+ 
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    volume_size           = var.root_device_size
+    volume_type           = var.root_device_type
+  }
+ 
+  tags = {
+    "Owner"               = var.owner
+    "Name"                = "${var.owner}-instance"
+    "KeepInstanceRunning" = "false"
+  }
+}
+
+
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "kp" {
+  key_name   = var.key_pair       # Create "myKey" to AWS!!
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./terraform_key.pem"
+  }
+}
